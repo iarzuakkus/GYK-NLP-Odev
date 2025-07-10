1ae56e16d6b80afe22c6005485f84deaa699c745
Arzu Akkus <ilaydaarzuakkus@gmail.com>
2025-07-10 16:33:35 +0300

    sentiment analysis

---
+++ ep3-0307/save_tokenizer.py
@@ -0,0 +1,14 @@
+import pickle
+from preprocess import preprocess_data  # tokenizer zaten burada oluşuyor
+
+if __name__ == "__main__":
+    csv_path = "data/goemotions_merged.csv"
+
+    # Sadece tokenizer'ı almak için çalıştırıyoruz
+    _, _, _, _, tokenizer, _ = preprocess_data(csv_path)
+
+    # Kaydet
+    with open("tokenizer.pkl", "wb") as f:
+        pickle.dump(tokenizer, f)
+
+    print("✅ Tokenizer başarıyla kaydedildi: tokenizer.pkl")
